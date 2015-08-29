f8713a0c4b699e2949b6808b2fecab8edd8b0079
zhanghonged <zhanghonged@126.com>
2015-08-29 11:07:46 +0800

    kill openfire

--- appdeploy.py
+++ appdeploy.py
@@ -4,8 +4,8 @@
 passwords={'root:192.168.1.5:22':'hongpan'}
 
 #kill openfire进程
-#cmds=["ps -ef | grep openfire | grep -v grep |awk '{print $2}'","ps -ef | grep openfire | grep -v grep |awk '{print $2}'|xargs kill -9"]
+cmds=["ps -ef | grep openfire | grep -v grep |awk '{print $2}'","ps -ef | grep openfire | grep -v grep |awk '{print $2}'|xargs kill -9"]
 
 #启动openfire
-cmds=['source /etc/profile; /opt/openfire/bin/openfirectl start']
+#cmds=['source /etc/profile; /opt/openfire/bin/openfirectl start']
 
