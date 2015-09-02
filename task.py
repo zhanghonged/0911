544348b327ca7c2a91327956ce90a81c173e9212
zhanghonged <zhanghonged@126.com>
2015-09-03 05:30:55 +0800

    修改task.py 采用class实现功能

--- task.py
+++ task.py
@@ -5,46 +5,101 @@ import sys
 import os
 import appdeploy
 
-
-
-def run(user,passw,host,port,cmds):
-  ssh = paramiko.SSHClient()
-  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  ssh.connect(host,port=port,username=user,password=passw)
-  #print type(cmds)
-  if isinstance(cmds,list):
-    for i in xrange(len(cmds)):
-      print 'cmd is "%s" **********************************************' %cmds[i] 
-      stdin,stdout,stderr=ssh.exec_command(cmds[i])
+class run_cmd():
+  def __init__(self,user,passw,host,port,cmds):
+    self.user=user
+    self.passw=passw
+    self.host=host
+    self.port=port
+    self.cmds=cmds
+    print user,passw,host,port,cmds
+  def run(self):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(self.host,port=self.port,username=self.user,password=self.passw)
+    #print type(cmds)
+    #如果命令是列表形式
+    if isinstance(self.cmds,list):
+      for i in xrange(len(self.cmds)):
+        print 'cmd is "%s" **********************************************' %self.cmds[i]
+        stdin,stdout,stderr=ssh.exec_command(self.cmds[i])
+        for i in stdout.readlines():
+          print i,
+        for i in stderr.readlines():
+          print i,
+    else:
+      stdin,stdout,stderr=ssh.exec_command(self.cmds)
       for i in stdout.readlines():
         print i,
       for i in stderr.readlines():
-        print i,
-  else:
-    stdin,stdout,stderr=ssh.exec_command(cmds)
-    for i in stdout.readlines():
-      print i,
-  ssh.close()
+        print i
+    ssh.close()
 
-def tran_put(user,passw,host,port,local,remote):
-  t = paramiko.Transport((host,port))
-  t.connect(username=user,password=passw)
-  sftp = paramiko.SFTPClient.from_transport(t)
-  localpath = local
-  remotepath = os.path.join(remote,local.split('/')[-1])
-  sftp.put(localpath,remotepath)
-  t.close()
+#t=run_cmd('root','hongpan','192.168.1.5',22,'ifconfig br0')
+#t.run()
 
-def tran_get(user,passw,host,port,remote,local):
-  t = paramiko.Transport((host,port))
-  t.connect(username=user,password=passw)
-  sftp = paramiko.SFTPClient.from_transport(t)
-  remotepath = remote
-  localpath = os.path.join(local,remotepath.split('/')[-1])
-  print localpath
-  sftp.get(remotepath,localpath)
-  t.close()
+class uad():
+  def __init__(self,user,passw,host,port,local,remote):
+    self.user=user
+    self.passw=passw
+    self.host=host
+    self.port=port
+    self.local=local
+    self.remote=remote
+    
+  def tran_put(self):
+    t = paramiko.Transport((self.host,self.port))
+    t.connect(username=self.user,password=self.passw)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    localpath = self.local
+    remotepath = os.path.join(self.remote,self.local.split('/')[-1])
+    sftp.put(localpath,remotepath)
+    t.close()
 
+  def tran_get(self):
+    t = paramiko.Transport((self.host,self.port))
+    t.connect(username=self.user,password=self.passw)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    remotepath = self.local
+    localpath = os.path.join(self.remote,remotepath.split('/')[-1])
+    print 'remotepath is %s' %remotepath
+    print 'localpath is %s' %localpath
+    sftp.get(remotepath,localpath)
+    t.close()
+
+
+
+def exect(minfo,ccc):
+  for j,k in minfo.items():
+    user=j.split(':')[0]
+    host=j.split(':')[1]
+    port=int(j.split(':')[2])
+    password=k
+    print 'exec host is                                                %s ' %host
+    if len(sys.argv) > 1:
+      if sys.argv[1] == 'run' and len(sys.argv) == 3:
+        print sys.argv[1:]
+        r=run_cmd(user,password,host,port,sys.argv[2])
+        r.run()
+      elif sys.argv[1] == 'put' and len(sys.argv) == 4:
+        print sys.argv[1:]
+        p=uad(user,password,host,port,sys.argv[2],sys.argv[3])
+        p.tran_put()
+      elif sys.argv[1] == 'get' and len(sys.argv) == 4:
+        print sys.argv[1:]
+        p=uad(user,password,host,port,sys.argv[2],sys.argv[3])
+        p.tran_get()
+      else:
+        print 'Is Wrong,Please input again'
+    else:
+      r=run_cmd(user,password,host,port,ccc)
+      r.run()
+
+exect(appdeploy.passwords,appdeploy.cmds)
+
+
+'''
+#下面是用关键字参数实现exect函数的代码片段 **kwargs
 def exect(minfo,**kw):
   for j,k in minfo.items():
     user=j.split(':')[0]
@@ -78,3 +133,4 @@ def zhixing():
     exect(appdeploy.passwords,cmds=appdeploy.cmds)
 
 zhixing()
+'''
